d2ed1a4aa5a763c88173a42163fe384c8d5009e9
Vasubabu <vasubabu@licious.in>
2018-09-30 13:21:10 +0530

    first commit

---
+++ src/main/java/com/licious/domain/Product.java
@@ -0,0 +1,53 @@
+package com.licious.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+/**
+ * Created by ahmed on 30.5.18.
+ */
+
+@Entity
+public class Product implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Version
+    private Integer version;
+
+    @NotEmpty
+    private String name;
+
+    private boolean available;
+
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+
+
+}
---
+++ src/main/java/com/licious/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.licious.repository;
+
+import com.licious.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by ahmed on 30.5.18.
+ */
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    Product findByNameLike(String name);
+    List<Product> findAll();
+}
